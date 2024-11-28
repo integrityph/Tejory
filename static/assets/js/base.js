e3491d3fdd8ff15f81490edf8d004288192cce8d
arnadame <adameaaron25@gmail.com>
2024-11-28 10:59:30 +0800

    updated getting started page

--- static/assets/js/base.js
+++ static/assets/js/base.js
@@ -115,3 +115,24 @@ var navsettings = document.querySelector('#nav-setings>*')
     }
   
   }
+
+const cards = document.querySelectorAll(".card");
+const wrapper = document.querySelector(".cards");
+
+wrapper.addEventListener("mousemove", (event) => {
+  cards.forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    card.style.setProperty("--xPos", `${x}px`);
+    card.style.setProperty("--yPos", `${y}px`);
+  });
+})
+
+  var btns = document.querySelectorAll('.btn-next')
+
+  btns.forEach((e, index) => {
+    e.addEventListener('click', () => {
+      cards[index + 1].classList.remove('hidden')      
+    })
+  });
