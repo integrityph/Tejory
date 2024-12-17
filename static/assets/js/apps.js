9e5eb327b108e8984208aaccc2ea60e6e2e84ccb
arnadame <adameaaron25@gmail.com>
2024-12-17 15:48:20 +0800

    updated apps page

---
+++ static/assets/js/apps.js
@@ -0,0 +1,53 @@
+function getOS() {
+  const userAgent = window.navigator.userAgent,
+      platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+      macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+  let os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (/Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+var appLink = document.querySelector('#AppLink')
+var redirectLink = document.querySelector('#redirectLink')
+var counter = 5;
+
+if(getOS() == 'Linux'){
+  appLink.href = "https://integrityph.github.io/Tejory/"
+} else {
+  appLink.href = "https://integrityph.github.io/Tejory/"
+}
+
+setInterval(function() {
+  counter--;
+
+  if (counter >= 0) {
+    span = document.getElementById("counter");
+    span.innerHTML = counter;
+  }
+
+  if (counter === 0) {
+    redirectLink.classList.remove('invisible')
+    clearInterval(counter);
+  }
+}, 1000);
+
+window.setTimeout(function() {
+  window.location.href = appLink.href;
+}, 5000);
+
+// window.location.replace("http://www.w3schools.com");
+// alert(getOS());
